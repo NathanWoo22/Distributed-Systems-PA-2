e9dd5684e5805da56c6f296fd0ca561442c5e28f
colinbrush <97994812+colinbrush@users.noreply.github.com>
2024-09-21 15:30:06 -0600

    moving to funcitonal

---
+++ DAP2/DAP2_BrushWoo/functional.py
@@ -0,0 +1,24 @@
+def main():
+    print("Hello World!")
+
+
+def GlobalInitialize():
+    return 
+
+def QuitAndCleanup():
+    return
+
+def MInitailize():
+    return
+
+def MLockMutex():
+    return
+
+def MReleaseMutex():
+    return
+
+def MCleanup():
+    return
+
+if __name__ == "__main__":
+    main()
