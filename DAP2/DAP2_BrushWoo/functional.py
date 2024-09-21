884e7dbcdb847d1aec893132ed12b0c857ca969c
colinbrush <97994812+colinbrush@users.noreply.github.com>
2024-09-21 16:51:13 -0600

    barebones message sending

--- DAP2/DAP2_BrushWoo/functional.py
+++ DAP2/DAP2_BrushWoo/functional.py
@@ -1,3 +1,6 @@
+import threading
+from socket import *
+
 def main():
     print("Hello World!")
 
@@ -5,37 +8,65 @@ class Maekawa():
     def __init__():
         return
     
-    #this host is an int and hosts is an array of pairs (ip, port)
+    #thishost is an int and hosts is an array of pairs (ip, port)
     def GlobalInitialize(self, thishost, hosts):
         self.numProcess = len(hosts)
         self.myNum = thishost
         self.processes = hosts
+        self.vecClock = [0]*len(hosts)
+        self.clockLock = threading.Lock()
+        self.myAcks = [False]*len(hosts)
+        self.acksLock = threading.Lock()
+        self.myReleases = [False]*len(hosts)
+        self.releLock = threading.Lock()
+        self.myRequests = [False]*len(hosts)
+        self.requLock = threading.Lock()
+        self.sendSocket = socket(AF_INET,SOCK_DGRAM)
+        #Last thing to happen
+        self.listenThread = threading.Thread(target= self.Listen, daemon=True)
+        self.listenThread.start()
         return 
 
-    def CreateSubsets():
+    def CreateSubsets(self):
         # Determine size of the grid 
         return
 
-    def QuitAndCleanup():
+    def QuitAndCleanup(self):
         return
 
-    def MInitailize():
+    def MInitailize(self):
         return
 
-    def MLockMutex():
+    def MLockMutex(self):
         return
 
-    def MReleaseMutex():
+    def MReleaseMutex(self):
         return
 
-    def MCleanup():
+    def MCleanup(self):
+        return
+    
+    #Should be started in a seperate thread
+    def Listen(self):
+        listenSocket = socket(AF_INET, SOCK_DGRAM)
+        listenSocket.bind(('',), self.hosts[self.myNum][1])
+        while 1:
+            message, clientAddress = listenSocket.recvfrom(4096)
+
         return
 
     #sendID should be integer corresponding to desired process to send too
     #Message should be an integer 0 for Ack 1 for Request 2 for Release
-    def MessageHandle(sendId, Message):
+    #Handles self.vecClock intrementing
+    def MessageSending(self, sendId, Message):
         #Header: Self Process ID (Location in inital Array) self clock end Header value
         #Message Either Ack or Request
+        self.clockLock.acquire()
+        self.vecClock[self.myNum] = self.vecClock[self.myNum] + 1
+        sendAddress, sendPort = self.hosts[sendId]
+        composed = f"{self.myNum},{self.vecClock[self.myNum],Message}".encode()
+        self.sendSocket.sendto(composed, (sendAddress,sendPort))
+        self.clockLock.release()
         return
 
 if __name__ == "__main__":
