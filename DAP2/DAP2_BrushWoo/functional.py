936e13de88fda862c86ef0e7a57f5f59c7714fd3
colinbrush <97994812+colinbrush@users.noreply.github.com>
2024-09-21 15:48:11 -0600

    MessageHandle

--- DAP2/DAP2_BrushWoo/functional.py
+++ DAP2/DAP2_BrushWoo/functional.py
@@ -20,5 +20,12 @@ def MReleaseMutex():
 def MCleanup():
     return
 
+#sendID should be integer corresponding to desired process to send too
+#Message should be an integer 0 for Ack 1 for Request 2 for Release
+def MessageHandle(sendId, Message):
+    #Header: Self Porcess ID (Location in inital Array) self clock end Header value
+    #Message Either Ack or Request
+    return
+
 if __name__ == "__main__":
     main()
