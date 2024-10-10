738d15f7f7f361e729e0976af4c34946b9d47f7a
Nathan Woo <nathanwoo22@gmail.comm>
2024-10-10 00:42:07 -0600

    progress

--- functional.py
+++ functional.py
@@ -2,6 +2,7 @@ import numpy as np
 import copy
 import threading
 from socket import *
+import socket as skt
 import heapq
 import time
 import logging
@@ -16,6 +17,7 @@ class Maekawa():
     #thishost is an int and hosts is an array of pairs (ip, port)
     def GlobalInitialize(self, thishost, hosts):
         self.logger = logging.getLogger(__name__)
+        
         logging.basicConfig(filename='debug_log.log', encoding='utf-8', level=logging.DEBUG)
         self.numProcess = len(hosts)
         self.hosts = hosts
@@ -39,7 +41,36 @@ class Maekawa():
         self.listenSocket.bind(('', self.processes[self.myNum][1]))
         self.listenThread = threading.Thread(target= self.Listen, daemon=True)
         self.listenThread.start()
+        self.readyNodes = [False] * len(hosts)
+        self.wait_for_all_servers_ready()
+        self.CreateSubsets()
         return 
+    
+    def is_server_ready(self, host, port):
+        """Attempt to connect to a server and check if it's ready."""
+        try:
+            # Try to create a socket connection to the given host and port
+            # print(f"sending message to {host}, {port}")
+            self.sendSocket.sendto(("checking_ready " + str(self.myNum)).encode('utf-8'), (host, port))
+
+        except Exception as e:
+            # Return False if connection is refused or timed out
+            return False
+
+    def wait_for_all_servers_ready(self):
+        """Keep checking if all servers are ready by pinging their ports."""
+        while True:
+            all_ready = True
+            for server in self.hosts:
+                host, port = server
+                self.is_server_ready(host, port)
+            for node in self.readyNodes:
+                if node == False:
+                    all_ready = False
+            if all_ready:
+                # print("all nodes are ready!!!")
+                break
+            time.sleep(2)  # Wait for a short time before checking again
 
     def CreateSubsets(self):
 
@@ -100,6 +131,8 @@ class Maekawa():
         while True:
             allAckedFlag = True
             # Check  that each process has sent ack before returning
+            print(self.myAcks)
+            time.sleep(0.5)
             for i, host in enumerate(self.subset):
                 if host - 1 == self.myNum:
                     continue
@@ -125,7 +158,20 @@ class Maekawa():
         return
 
     def MCleanup(self):
-        while self.myRequests: time.sleep(1)
+        # Wait until all pending requests are processed
+        count = 0
+        while True:
+        
+            self.requLock.acquire()
+            pending_requests = len(self.myRequests) > 0
+            self.requLock.release()
+            if pending_requests == False and count >= 5: 
+                break
+            else:
+                count += 1
+                time.sleep(1)
+            time.sleep(1)  # Wait a bit before checking again
+        print("No more requests or pending acknowledgments. Cleanup can proceed.")
         return
     
     def receiveRequest(self, processID, curClock):
@@ -139,7 +185,7 @@ class Maekawa():
         if self.voteGiven == False:
             self.voteGiven = True
             requestClock, processRequestAcked = heapq.heappop(self.myRequests)
-            thread = threading.Thread(target=self.MessageSending(processRequestAcked - 1, 0))
+            thread = threading.Thread(target=self.MessageSending(processRequestAcked + 1, 0))
             thread.start()
             thread.join()
         self.voteGivenLock.release()
@@ -208,19 +254,30 @@ class Maekawa():
                 curClock = copy.copy(self.vecClock)
                 self.clockLock.release()
                 if messageVal == 0:
-                    self.logger.info(f"Received Ack from {processId}")
+                    self.logger.info(f"{self.myNum} Received Ack from {processId}")
                     self.acksLock.acquire()
                     self.myAcks[processId] = True
                     self.acksLock.release()
                 elif messageVal == 1:
-                    self.logger.info(f"Received Request from {processId}")
+                    self.logger.info(f"{self.myNum} Received Request from {processId}")
                     self.receiveRequest(processId, curClock)
                 elif messageVal == 2:
-                    self.logger.info(f"Received Release from {processId}")
+                    self.logger.info(f"{self.myNum} Received Release from {processId}")
                     self.receiveRelease(processId, curClock)
+                
+            elif " " in composed:
+                # message, clientAddress = self.listenSocket.recvfrom(4096)
+                # print(f"Got {message} from {clientAddress}")
+                received_from = int(composed[-1])
+                # print(f"Sending {self.myNum} to {self.hosts[received_from]}")
+                self.listenSocket.sendto((str(self.myNum)).encode('utf-8'), self.hosts[received_from])
                     # self.releLock.acquire()
                     # self.myReleases.append(processId)
                     # self.releLock.release()
+            else:
+                # print(f"Received {message}!")
+                self.readyNodes[int(message)] = True
+
         return
 
     #sendID should be integer corresponding to desired process to send too
@@ -231,12 +288,12 @@ class Maekawa():
         #Message Either Ack or Request
         self.clockLock.acquire()
         self.vecClock[self.myNum] = self.vecClock[self.myNum] + 1
-        sendAddress, sendPort = self.hosts[sendId - 1] # -1 to correct indexing error
+        sendAddress, sendPort = self.hosts[sendId -1 ] # -1 to correct indexing error
         messageClock = f"{self.vecClock[0]}"
         for i in range(1, len(self.vecClock)):
             messageClock = f"{messageClock},{self.vecClock[i]}"
         composed = f"{self.myNum} {Message} {messageClock}".encode()
-        self.logger.info(f"Sending message type {Message}: {(sendAddress,sendPort)}")
+        self.logger.info(f"{self.myNum} Sending message type {Message} to {sendId - 1} {(sendAddress,sendPort)}")
         print(f"{self.hosts[self.myNum][0]}:{self.vecClock}")
         self.sendSocket.sendto(composed, (sendAddress,sendPort))
         self.clockLock.release()
--- main.py
+++ main.py
@@ -11,9 +11,11 @@ import sys
 
 processid = int(sys.argv[1])
 maekawa =Maekawa()
-maekawa.GlobalInitialize(processid, [("10.60.88.67", 5555), ("10.60.88.67", 5556)])
-maekawa.CreateSubsets()
+# maekawa.GlobalInitialize(processid, [("localhost", 5555), ("localhost", 5556), ("localhost", 5557), ("localhost", 5558), ("localhost", 5559)])
+# maekawa.GlobalInitialize(processid, [("localhost", 5555), ("localhost", 5556), ("localhost", 5557)])
+maekawa.GlobalInitialize(processid, [("localhost", 5555), ("localhost", 5556)]) 
 maekawa.MInitailize()
+# time.sleep(5)
 maekawa.MLockMutex()
 print(f"Process {processid} entering critical section")
 maekawa.MReleaseMutex()
